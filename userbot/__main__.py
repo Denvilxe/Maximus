4d27aa37c40182f04399fe37b7316d29efc23d0a
Denvilxe <90274962+Denvilxe@users.noreply.github.com>
2021-12-28 23:59:40 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -12,8 +12,8 @@ for module_name in ALL_MODULES:
     imported_module = importlib.import_module("userbot.modules." + module_name)
 
 LOGS.info("Your Bot is alive! Test it by typing .alive or .ping on any chat."
-          "Should you need assistance, head to https://t.me/TheBotSupport")
-LOGS.info("Your Bot Version is 2.2 By @noobanon")
+          "Should you need assistance, head to https://t.me/Aniebotsupports")
+LOGS.info("Your Bot Version is 2.2 By @Denvil_pro")
 
 if len(sys.argv) not in (1, 3, 4):
     bot.disconnect()
